6dc9a4633a2782815d3717a80ca1527d6983e518
Mike Green <nekbet@gmail.com>
2014-07-02 15:48:11 -0700

    updated with filter on distance

--- ddl/distance-notif.sql
+++ ddl/distance-notif.sql
@@ -6,13 +6,18 @@ SELECT u.user_id, u.username, u.user_email, u.user_notify_type,	t.pnp_sendZip , 
 60 * degrees(acos(sin(radians(airports.lat)) * sin(radians(sendzip.lat)) + cos(radians(airports.lat)) * cos(radians(sendzip.lat)) * cos(radians(airports.lon - sendzip.lon)))) + 
 60 *  degrees(acos(sin(radians(airports.lat)) * sin(radians(reczip.lat)) + cos(radians(airports.lat)) * cos(radians(reczip.lat)) * cos(radians(airports.lon - reczip.lon)))) - 
 60 *  degrees(acos(sin(radians(reczip.lat)) * sin(radians(sendzip.lat)) + cos(radians(reczip.lat)) * cos(radians(sendzip.lat)) * cos(radians(reczip.lon - sendzip.lon)))) as dist
+,  ( 3959 * acos( cos( radians(reczip.lat) ) 
+   * cos( radians( sendzip.lat ) ) 
+   * cos( radians(sendzip.lon) - radians(reczip.lon)) + sin(radians(reczip.lat)) 
+   * sin( radians(sendzip.lat)))) AS distance 
+
 			FROM    phpbb_users u,
 				phpbb_profile_fields_data p,
 				phpbb_topics t,
 				zipcodes sendzip,
 				zipcodes reczip,
 				airports
-			WHERE 	t.topic_id = 24719 and
+			WHERE 	t.topic_id = 28800 and
 				p.pf_flying_radius > 0 and 
 				p.pf_pilot_yn =1 and airports.apt_id = p.pf_airport_id and 
 				sendzip.zip = t.pnp_sendZip and
@@ -21,3 +26,11 @@ SELECT u.user_id, u.username, u.user_email, u.user_notify_type,	t.pnp_sendZip , 
 				60 * degrees(acos(sin(radians(airports.lat)) * sin(radians(sendzip.lat)) + cos(radians(airports.lat)) * cos(radians(sendzip.lat)) * cos(radians(airports.lon - sendzip.lon)))) + 
 60 *  degrees(acos(sin(radians(airports.lat)) * sin(radians(reczip.lat)) + cos(radians(airports.lat)) * cos(radians(reczip.lat)) * cos(radians(airports.lon - reczip.lon)))) - 
 60 *  degrees(acos(sin(radians(reczip.lat)) * sin(radians(sendzip.lat)) + cos(radians(reczip.lat)) * cos(radians(sendzip.lat)) * cos(radians(reczip.lon - sendzip.lon)))) < p.pf_flying_radius
+and 
+( 3959 * acos( cos( radians(reczip.lat) ) 
+   * cos( radians( sendzip.lat ) ) 
+   * cos( radians(sendzip.lon) - radians(reczip.lon)) + sin(radians(reczip.lat)) 
+   * sin( radians(sendzip.lat)))) < 1000
+
+
+-- select MBRContains();
