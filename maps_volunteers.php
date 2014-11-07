10b8664260fdbab1dc01cc856ff2b005830f7ee8
Mike Green <nekbet@gmail.com>
2014-11-07 10:51:54 -0600

    fixed infowindow placement

--- maps_volunteers.php
+++ maps_volunteers.php
@@ -125,7 +125,8 @@
 			google.maps.event.addListener(volunteerMarker, 'click', function(event) {
 				// get the click's latlng and use that as anchor for infoWindow
 					var marker = new google.maps.Marker({
-						position: event.latLng
+						position: event.latLng,
+						map: map
 						}); 
 					
 				// set the info popup content as the html from polyline above, then open it
