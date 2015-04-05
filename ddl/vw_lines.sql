a47051d9fa294812f8c56bd8d2078c6eee89d60e
Mike Green <nekbet@gmail.com>
2015-04-05 16:15:40 -0700

    added lat/long difference calc

--- ddl/vw_lines.sql
+++ ddl/vw_lines.sql
@@ -1,7 +1,7 @@
 /* this view supports create_xml.php 
 it provides the topic and sending/receiving lat/lon for forum 5, the request forum
 */
-create view vw_lines
+alter view vw_lines
 AS
 select date_add('1969-12-31 20:00:00', INTERVAL t.topic_last_post_time SECOND ) as last_post,
     DATE_FORMAT(date_add('1969-12-31 20:00:00', INTERVAL t.topic_last_post_time SECOND )
@@ -9,7 +9,9 @@ select date_add('1969-12-31 20:00:00', INTERVAL t.topic_last_post_time SECOND ) 
     t.topic_id, t.topic_title,
     t.pnp_sendZip, z_send.lat as sendLat, z_send.lon as sendLon, t.pnp_recZip, z_rec.lat as recLat, z_rec.lon as recLon,
     concat(z_send.city,', ',z_send.state) as sendCity,
-    concat(z_rec.city,', ',z_rec.state) as recCity
+    concat(z_rec.city,', ',z_rec.state) as recCity,
+    abs(z_send.lat - z_rec.lat) AS diffLat,
+    abs(z_send.lon - z_rec.lon) AS diffLon
 from phpbb_topics t 
     LEFT OUTER JOIN zipcodes z_send on t.pnp_sendZip = z_send.zip 
     LEFT OUTER JOIN zipcodes z_rec on t.pnp_recZip = z_rec.zip 
