691254bbe7e466a400c217de777e43f09dd947d3
Mike Green <nekbet@gmail.com>
2016-11-30 09:17:41 -0500

    template for adding missing zips

---
+++ ddl/insert_zip_code.sql
@@ -0,0 +1,2 @@
+INSERT INTO zipcodes (`id`, `zip`, `lat`, `lon`, `city`, `state`, `fullstate`, `county`, `zipclass`) 
+VALUES (NULL, '94070', '37.501', '-122.261', 'San Carlos', 'CA', 'California', 'US', 'STANDARD');
