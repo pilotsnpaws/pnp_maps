95a269a8fd058fd4e2030f420cb90a1a41dedf3e
Mike Green <nekbet@gmail.com>
2017-08-22 13:05:53 -0600

    Fix sorting and ordering. Add new email by zip.

--- maps_more/mapsmore_volunteersZipEmail.php
+++ maps_more/mapsmore_volunteersZipEmail.php
@@ -1,6 +1,8 @@
 <?php
-// created 2017-04-05 by Mike 
+// created 2015-05-29 by Mike 
+// updated to add email 2017-08-22 by Mike
 // this is temporary as we can't expose emails to a public page - will move into phpbb format asap
+// this should allow someone to search pilots within a distance of a zip code 
 
 // include the phpbb forum config for db connection
 include ( "../forum/config.php");
@@ -13,24 +15,23 @@ if (mysqli_connect_errno())
     echo "mySQL connect failed: " . mysqli_connect_error() . $lineBreak ;
     }
 
-    $airportCode = isset($_GET['airportCode']) ? $_GET['airportCode'] : '';
+    $zipCode = isset($_GET['zipCode']) ? $_GET['zipCode'] : '';
     $miles = isset($_GET['miles']) ? $_GET['miles'] : '';
     $debug = isset($_GET['debug']) ? $_GET['debug'] : '';
 
     // set a default if nothing provided
-    if ($airportCode == "" ) {
-                       $airportCode = 'KMGY' ;
+    if ($zipCode == "" ) {
+                       $zipCode = '45409' ;
                        }
     if ($miles == "" ) {
                        $miles = '50' ;
                        }
 
 ?>
-
     <html>
 
     <head>
-        <title>PNP|Volunteers around an airport</title></head>
+        <title>PNP|Volunteers around a zipcode</title></head>
         <link rel="shortcut icon" href="/forum/favicon.ico">
     </head>
 
@@ -38,8 +39,6 @@ if (mysqli_connect_errno())
         table, th, td {
             border: 1px solid black;
             border-collapse: collapse;
-            font-family: Arial,Verdana,sans-serif;
-            font-size: 13px;            
         }
         th, td {
             padding: 5px;
@@ -48,6 +47,8 @@ if (mysqli_connect_errno())
         .TFtable{
                 width:100%; 
                 border-collapse:collapse; 
+                font-family: Arial,Verdana,sans-serif;
+                font-size: 13px;
             }
             .TFtable td{ 
                 padding:7px; border:#4e95f4 1px solid;
@@ -74,11 +75,11 @@ if (mysqli_connect_errno())
 
     <br>
         <div id="optionsBox">
-            <form method="GET" action="mapsmore_volunteersAirportEmail.php" id="searchform">
-            Search for pilots near airport: <input type="text" name="airportCode" id="airportCode" maxlength="4" size="11" placeholder="Airport Code" value="<?
+            <form method="GET" action="mapsmore_volunteersZip.php" id="searchform">
+            Search for pilots near zip code: <input type="text" name="zipCode" id="zipCode" maxlength="5" size="8" placeholder="Zip Code" value="<?
 
     // prefill the field with the prior search
-    echo $airportCode ; 
+    echo $zipCode ; 
     
     ?>"/>  
             Radius to show:  
@@ -91,9 +92,6 @@ if (mysqli_connect_errno())
                     <option value="150">150 miles</option>
                     <option value="200">200 miles</option>
                     <option value="300">300 miles</option>
-                    <option value="400">400 miles</option>
-                    <option value="500">500 miles</option>
-									
                 </select>
             <input type="submit" name="submit" value="Search" />
             </form>
@@ -101,19 +99,17 @@ if (mysqli_connect_errno())
 
 <?
 
-    echo 'Showing pilots within ' . $miles . ' miles of airport code ' . $airportCode ;
+    echo 'Showing pilots within ' . $miles . ' miles of zip code ' . $zipCode ;
     echo $lineBreak ; 
 
-    $query = 'select user_id, username, user_email, pf_flying_radius, fn_distance(a.lat, a.lon, v.lat,v.lon) as distance, '
-    . ' a.apt_id AS from_apt, a.apt_id, a.apt_name, a.city, last_visit_human, v.apt_id AS vol_apt_id, ' 
-		.	' v.apt_name AS vol_apt_name, v.city AS vol_city, v.user_inactive_reason ' 
+    $query = 'select v.user_id, v.username, v.user_email, v.pf_flying_radius, fn_distance(a.lat, a.lon, v.lat,v.lon) as distance, '
+    . ' v.apt_id as from_apt, v.apt_id, v.apt_name, v.city, v.last_visit_human '
     . ' from vw_volunteers v, '
-    . ' airports a ' 
-    . ' where pf_pilot_yn = 1 '  // only show pilots
-    . ' and a.apt_id = "' . $airportCode . '" '
-    . ' and fn_distance(a.lat, a.lon, v.lat,v.lon) < ' . $miles 
-		. ' and user_inactive_reason = 0 ' // only show active users
-    . ' order by distance,last_visit_human ' ; 
+    . ' zipcodes a '
+    . ' where pf_pilot_yn = 1 '
+    . ' and a.zip = "' . $zipCode . ' " '
+    . ' and fn_distance(a.lat, a.lon, v.lat,v.lon) < ' . $miles
+    . ' order by distance, last_visit_human ' ;
 
     if ( $debug == 'yes') echo 'Debug: Yes' . $lineBreak . $lineBreak ;
 	if ( $debug == "yes") echo $query . $lineBreak;
@@ -127,27 +123,28 @@ if (mysqli_connect_errno())
     $colBreak = '</td><td>' ; 
 
     echo '<table class="TFtable" border="1">' ;
-    echo '<tr><th>User' . $headerBreak . 'Email' . $headerBreak . 'Willing to fly' . $headerBreak . 'Miles from ' . 
-			$airportCode . '</th><th colspan=2>' .  'Home Airport' . $headerBreak . 'Airport City' . 
-			$headerBreak . 'Last active on forum' . '</th></tr>' ; 
+    echo '<tr><th>User' . $headerBreak . 'Email' . $headerBreak . 'Willing to fly' . $headerBreak 
+        . 'Miles from ' . $zipCode . '</th><th colspan=2>' .  'Home Airport' . $headerBreak 
+        . 'Airport City' . $headerBreak 
+        . 'Last active on forum' . '</th></tr>' ; 
 
     while($row = mysqli_fetch_array($result)) {
         $rows[] = $row;
         $user_id = $row['user_id'];
+        $email   = $row['user_email'];
         $username = $row['username'];
-		$email	 = $row['user_email'];
         $flying_radius = $row['pf_flying_radius'];
         $distance = $row['distance'];
         $airportCodeFrom = $row['from_apt'];
-        $airportCode = $row['vol_apt_id'];
-        $airportName = $row['vol_apt_name'];
-        $airportCity = $row['vol_city'];
+        $airportCode = $row['apt_id'];
+        $airportName = $row['apt_name'];
+        $airportCity = $row['city'];
         $lastVisitDate = $row['last_visit_human'];
-        echo '<tr><td><a href="/forum/memberlist.php?mode=viewprofile&u=' . 
-						$user_id . '" target=_blank>' . $username . '</a>' . $colBreak . $email .
-						$colBreak . $flying_radius . 
-						$colBreak . $distance . $colBreak . '<a href="https://www.aopa.org/airports/' . 
-						$airportCode . '" target="_blank">' . $airportCode . '</a>' . $colBreak . $airportName . 
+        echo '<tr><td><a href="/forum/memberlist.php?mode=viewprofile&u=' . $user_id . '" target=_blank>' . $username . '</a>' . 
+            $colBreak . $email . 
+            $colBreak . $flying_radius . $colBreak . $distance . 
+            $colBreak . '<a href="https://www.aopa.org/airports/' . 
+            $airportCode . '" target="_blank">' . $airportCode . '</a>' . $colBreak . $airportName . 
             $colBreak . $airportCity . $colBreak . $lastVisitDate . '</td></tr>' ;
 
     }
