f117320997eb1bf68d8472759f4dc0bc37a5ff38
Mike Green <nekbet@gmail.com>
2018-01-17 09:08:15 -0700

    updated sql

--- ddl/set_flying_radius_0_old_users.sql
+++ ddl/set_flying_radius_0_old_users.sql
@@ -5,5 +5,5 @@ update phpbb_profile_fields_data
 set pf_flying_radius = 0
 where pf_flying_radius > 0
 and user_id IN
-(select u.user_id from phpbb_users u join phpbb_profile_fields_data p on u.user_id = p.user_id 
-		where user_lastvisit < 1414800000 and user_regdate < 1414800000 and pf_flying_radius > 0)
+(select u.user_id from phpbb_users u 
+where user_lastvisit < 1484668328 and user_regdate < 1484668328 )
