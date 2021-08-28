2d7aa7384d0c684ecb27f5c22a8612e79a305e91
Mike Green <772413+mikegreen@users.noreply.github.com>
2021-08-27 19:35:49 -0600

    updated queries

--- ddl/helpful_queries.sql
+++ ddl/helpful_queries.sql
@@ -4,3 +4,28 @@ select date_format(last_visit, '%Y %m'), count(*)
 from vw_volunteers 
 group by date_format(last_visit, '%Y %m')
 order by 1,2
+
+-- Deactivate users who haven't logged in or posted in last 18 months
+select u.user_id from phpbb_users u 
+where 1=1
+	and user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jul 01 2019 10:00PM', '%M %d %Y %h:%i%p'))
+	and user_regdate < UNIX_TIMESTAMP(STR_TO_DATE('Jul 01 2019 10:00PM', '%M %d %Y %h:%i%p'))
+    and user_type = 0 -- user_type = 0 means active ;
+    
+    select * from phpbb_users where user_id = 1108;
+-- SELECT UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2020 10:00PM', '%M %d %Y %h:%i%p'));
+
+update phpbb_users u
+	set user_type = 1 , user_inactive_reason = 3
+where 1=1
+	and user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jul 01 2019 10:00PM', '%M %d %Y %h:%i%p'))
+	and user_regdate < UNIX_TIMESTAMP(STR_TO_DATE('Jul 01 2019 10:00PM', '%M %d %Y %h:%i%p'))
+    and user_type = 0 -- user_type = 0 means active ;
+
+
+/* user_type
+define('USER_NORMAL', 0);
+define('USER_INACTIVE', 1);
+define('USER_IGNORE', 2);
+define('USER_FOUNDER', 3);
+*/
--- ddl/set_flying_radius_0_old_users.sql
+++ ddl/set_flying_radius_0_old_users.sql
@@ -1,9 +1,20 @@
 -- this query sets flying radius to 0 where the user hasnt visited in a long time
 
+-- first, run this with new dates to verify what you're doing
+-- ie, set this back 6 or 9 months https://www.unixtimestamp.com/index.php 
+select * from phpbb_profile_fields_data
+where pf_flying_radius > 0 
+and user_id IN
+(select u.user_id from phpbb_users u 
+where user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2021 10:00PM', '%M %d %Y %h:%i%p'))  and user_type = 0 );
+
+select * from phpbb_users u 
+where user_lastvisit < 1609484401 and user_regdate < 1622527201 and user_id = 13533;
+
 -- select * from update phpbb_profile_fields_data
 update phpbb_profile_fields_data
 set pf_flying_radius = 0
-where pf_flying_radius > 0
+where pf_flying_radius > 0  
 and user_id IN
 (select u.user_id from phpbb_users u 
-where user_lastvisit < 1484668328 and user_regdate < 1484668328 )
+where user_lastvisit < 1609484401 and user_regdate < 1609484401 );
