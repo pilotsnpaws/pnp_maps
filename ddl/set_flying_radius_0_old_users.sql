8ba69077019bd5da88427470cc328ad41b4939c1
Mike Green <772413+mikegreen@users.noreply.github.com>
2023-04-11 16:22:43 -0600

    Update set_flying_radius_0_old_users.sql

--- ddl/set_flying_radius_0_old_users.sql
+++ ddl/set_flying_radius_0_old_users.sql
@@ -1,23 +1,24 @@
 -- this query sets flying radius to 0 where the user hasnt visited in a long time
 
 -- first, run this with new dates to verify what you're doing
--- ie, set this back 6 or 9 months https://www.unixtimestamp.com/index.php 
+-- ie, this sets it for users who have not visited since before Jul 1 2022, and registered before Jan 1 just in case
 select * from phpbb_profile_fields_data
 where pf_flying_radius > 0 
 and user_id IN
 (select u.user_id from phpbb_users u 
-where user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2021 10:00PM', '%M %d %Y %h:%i%p'))  and user_type = 0 );
+where 1=1
+	and user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jul 01 2022 10:00PM', '%M %d %Y %h:%i%p'))
+	and user_regdate < UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2023 10:00PM', '%M %d %Y %h:%i%p'))
+	)
+order by user_id desc;
 
-select u.user_id from phpbb_users u 
-where user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2022 10:00PM', '%M %d %Y %h:%i%p')) 
-	and user_regdate < UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2022 10:00PM', '%M %d %Y %h:%i%p'));
-
-
--- select * from update phpbb_profile_fields_data
+-- do the update
 update phpbb_profile_fields_data
-set pf_flying_radius = 0
-where pf_flying_radius > 0  
-and user_id IN
-(select u.user_id from phpbb_users u 
-where user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Apr 01 2022 10:00PM', '%M %d %Y %h:%i%p')) 
-	and user_regdate < UNIX_TIMESTAMP(STR_TO_DATE('May 01 2022 10:00PM', '%M %d %Y %h:%i%p')) );
+	set pf_flying_radius = 0
+	where pf_flying_radius > 0  
+		and user_id IN
+			(select u.user_id from phpbb_users u 
+			where 1=1
+				and user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jul 01 2022 10:00PM', '%M %d %Y %h:%i%p'))
+				  and user_regdate < UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2023 10:00PM', '%M %d %Y %h:%i%p'))
+			  );
