b5e015328b5afe24d212c830a93954f36168927f
Mike Green <772413+mikegreen@users.noreply.github.com>
2023-04-12 10:09:35 -0600

    add zip code too, partial

--- ddl/remove_airport_code_inactive.sql
+++ ddl/remove_airport_code_inactive.sql
@@ -1,8 +1,20 @@
 -- first, copy the current airport ID to the backup field, 
 -- where the backup code is empty, but there is a present airport code
 update prod_forum.phpbb_profile_fields_data
-  set pf_airport_id_backup = upper(pf_airport_id)
-  where pf_airport_id_backup in (null, '') and pf_airport_id not in (null,'');
+	set pf_airport_id_backup = upper(pf_airport_id)
+	where 1=1
+		and (pf_airport_id_backup = '' or pf_airport_id_backup is null)
+		and (pf_airport_id != '' and pf_airport_id is not null)
+		and user_id < 100000;
+  
+-- then, do zip codes as well
+update prod_forum.phpbb_profile_fields_data
+	set pf_zip_code_backup = pf_zip_code
+	where 1=1
+		and (pf_zip_code_backup = '' or pf_zip_code_backup is null)
+		and (pf_zip_code != '' and pf_zip_code is not null)
+		and user_id < 100000;
+  
   
 -- set the airport code to null where the user has not logged in since the date specified
 update prod_forum.phpbb_profile_fields_data
