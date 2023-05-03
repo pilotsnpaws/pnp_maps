bc6cb9baf120b7a1490f5288a6136db840dbbe83
Mike Green <772413+mikegreen@users.noreply.github.com>
2023-05-03 17:33:24 -0600

    Update insert_zip_code.sql

--- ddl/insert_zip_code.sql
+++ ddl/insert_zip_code.sql
@@ -1,2 +1,14 @@
 INSERT INTO zipcodes (`id`, `zip`, `lat`, `lon`, `city`, `state`, `fullstate`, `county`, `zipclass`) 
 VALUES (NULL, '94070', '37.501', '-122.261', 'San Carlos', 'CA', 'California', 'US', 'STANDARD');
+
+
+INSERT INTO zipcodes
+(zip,
+lat,
+lon,
+city,
+state,
+fullstate)
+VALUES
+('11606', '18.4297', '-69.6689', 'Santo Domingo', 'DR', 'Dominican Republic' )
+;
