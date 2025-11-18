7b1233e80176e2c7d76b32f40cfe1a4b65f68477
Mike Green <772413+mikegreen@users.noreply.github.com>
2025-11-18 14:15:05 -0700

    Update SQL query to set flying radius for old users

--- ddl/set_flying_radius_0_old_users.sql
+++ ddl/set_flying_radius_0_old_users.sql
@@ -2,23 +2,21 @@
 
 -- first, run this with new dates to verify what you're doing
 -- ie, this sets it for users who have not visited since before Jul 1 2022, and registered before Jan 1 just in case
-select * from phpbb_profile_fields_data
-where pf_flying_radius > 0 
-and user_id IN
-(select u.user_id from phpbb_users u 
+select u.user_id from phpbb_users u JOIN phpbb_profile_fields_data ud ON u.user_id = ud.user_id
 where 1=1
-	and user_lastvisit 	< UNIX_TIMESTAMP(DATE_ADD(CURRENT_TIMESTAMP, INTERVAL -13 MONTH))
-	and user_regdate 	< UNIX_TIMESTAMP(DATE_ADD(CURRENT_TIMESTAMP, INTERVAL -13 MONTH))
-	)
-order by user_id desc;
+	and user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jul 01 2024 10:00PM', '%M %d %Y %h:%i%p'))
+      and user_regdate < UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2024 10:00PM', '%M %d %Y %h:%i%p'))
+	AND ud.pf_flying_radius > 0
+order by u.user_id desc;
 
 -- do the update
 update phpbb_profile_fields_data
-	set pf_flying_radius = 0
-	where pf_flying_radius > 0  
-		and user_id IN
-			(select u.user_id from phpbb_users u 
-			where 1=1
-				and user_lastvisit 	< UNIX_TIMESTAMP(DATE_ADD(CURRENT_TIMESTAMP, INTERVAL -13 MONTH))
-				and user_regdate 	< UNIX_TIMESTAMP(DATE_ADD(CURRENT_TIMESTAMP, INTERVAL -13 MONTH))
-	);
+set pf_flying_radius = 0
+where pf_flying_radius > 0  
+and user_id IN
+(select u.user_id from phpbb_users u JOIN phpbb_profile_fields_data ud ON u.user_id = ud.user_id
+where 1=1
+	and user_lastvisit < UNIX_TIMESTAMP(STR_TO_DATE('Jul 01 2024 10:00PM', '%M %d %Y %h:%i%p'))
+      and user_regdate < UNIX_TIMESTAMP(STR_TO_DATE('Jan 01 2024 10:00PM', '%M %d %Y %h:%i%p'))
+	AND ud.pf_flying_radius > 0
+  );
